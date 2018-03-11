095389182ac0bd250383fbc6a8899a12d68889a3
python大鹌鹑 <likanwen@icloud.com>
2018-03-11 21:23:35 +0800

    2018-03-11

--- 310.py
+++ 310.py
@@ -1,32 +1,20 @@
 class Solution:
     def findMinHeightTrees(self, n, edges):
-        graph, leaf, non_leaf = {}, set(), set()
+        graph,nodes = {},set(range(n))
         for start, end in edges:
             graph.setdefault(start, []).append(end)
             graph.setdefault(end, []).append(start)
-        start_leaf_node = None
-        for n in graph:
-            if len(graph[n]) == 1:
-                start_leaf_node = n if start_leaf_node is None else start_leaf_node
-                leaf.add(n)
-            else:
-                non_leaf.add(n)
-        from queue import Queue
-        visited, path_to_node = {start_leaf_node}, []
-        q = Queue()
-        q.put((start_leaf_node, (start_leaf_node,)))
-        while not q.empty():
-            start, cur_path = q.get()
-            flag = 0
-            for end in graph[start]:
-                if end not in visited:
-                    q.put((end, (*cur_path, end)))
-                    visited.add(end)
-                    flag += 1
-            if not flag:
-                path_to_node.append(cur_path)
-        print(path_to_node)
-
+        while len(nodes) > 2:
+            leaves = []
+            for start in graph:
+                if start not in nodes:
+                    continue
+                if len(graph[start]) == 1:
+                    leaves.append(start)
+            for leaf in leaves:
+                graph[graph[leaf][0]].remove(leaf)
+                nodes.remove(leaf)
+        return sorted(nodes)
 
 if __name__ == '__main__':
     solution = Solution()
