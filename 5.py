72dd1d3f34cf9f9b187e9a9d7d0b0154ae047ff9
李偘文 <leekanwen@gmail.com>
2018-04-18 13:41:13 +0800

    Update 5.py

--- 5.py
+++ 5.py
@@ -1,18 +1,21 @@
-# Longest Palindromic Substring
-
-# Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
-
-# Example:
-# Input: "babad"
-# Output: "bab"
-# Note: "aba" is also a valid answer.
-# Input: "cbbd"
-# Output: "bb"
-
-# Manacher's Algorithm !!!!
-
 class Solution:
     def longestPalindrome(self, s):
+        dp = [[1] * len(s) for _ in s]
+        for i in range(len(s) - 1, -1, -1):
+            for j in range(i + 1, len(s)):
+                if s[i] == s[j] and (j - i == 1 or dp[i + 1][j - 1] > 0):
+                    dp[i][j] = dp[i + 1][j - 1] + 2 if j - i > 1 else 2
+                else:
+                    dp[i][j] = 0
+        max_len, start, end = float('-inf'), None, None
+        for i in range(len(s)):
+            for j in range(i, len(s)):
+                if dp[i][j] > max_len:
+                    max_len = dp[i][j]
+                    start, end = i, j
+        return s[start:end + 1]
+    
+    def longestPalindrome1(self, s):
         s = '#' + '#'.join(s) + '#'
         RL = [0] * len(s)
         max_right = 0
@@ -36,26 +39,3 @@ class Solution:
                 max_length = RL[i]
 
         return max_substring[1::2]
-
-    def longestPalindrome1(self, s):
-        dp = [[1] * len(s) for _ in s]
-        for i in range(len(s) - 1, -1, -1):
-            for j in range(i + 1, len(s)):
-                if s[i] == s[j] and (j - i == 1 or dp[i + 1][j - 1] > 0):
-                    dp[i][j] = dp[i + 1][j - 1] + 2 if j - i > 1 else 2
-                else:
-                    dp[i][j] = 0
-        max_len, start, end = float('-inf'), None, None
-        for i in range(len(s)):
-            for j in range(i, len(s)):
-                if dp[i][j] > max_len:
-                    max_len = dp[i][j]
-                    start, end = i, j
-        return s[start:end + 1]
-
-
-if __name__ == '__main__':
-    print(Solution().longestPalindrome('cbbd'))
-    print(Solution().longestPalindrome1('cbbd'))
-    print(Solution().longestPalindrome('abba'))
-    print(Solution().longestPalindrome1('abba'))
