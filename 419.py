f8e63daebdbff2c8a9c9dd3e6a24374c9a360a6d
李偘文 <leekanwen@gmail.com>
2018-04-20 16:30:13 +0800

    Update 419.py

--- 419.py
+++ 419.py
@@ -1,8 +1,3 @@
 class Solution:
     def countBattleships(self, board):
-        res = 0
-        for i, row in enumerate(board):
-            for j, v in enumerate(row):
-                if v == 'X' and not ((i > 0 and board[i - 1][j] == 'X') or (j > 0 and board[i][j - 1] == 'X')):
-                    res += 1
-        return res
+        return sum(v == 'X' and not ((i > 0 and board[i - 1][j] == 'X') or (j > 0 and board[i][j - 1] == 'X')) for i, row in enumerate(board) for j, v in enumerate(row))
