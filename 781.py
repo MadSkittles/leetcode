45fb2120d38129e6279fb855cf73cb6a7f1f9598
李偘文 <leekanwen@gmail.com>
2018-04-23 17:32:33 +0800

    Create 781.py

---
+++ 781.py
@@ -0,0 +1,15 @@
+class Solution:
+    def numRabbits(self, answers):
+        from collections import Counter
+        res, c = 0, Counter(answers)
+        for k in c:
+            while c[k] > 0:
+                res += k + 1
+                c[k] -= k + 1
+        return res
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    print(solution.numRabbits([1, 1, 2]))
+    print(solution.numRabbits([10, 10, 10]))
