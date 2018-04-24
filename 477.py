3bdfb8fe012eaadcd954acf69e15a76213b88a11
李偘文 <leekanwen@gmail.com>
2018-04-24 19:52:53 +0800

    Update 477.py

--- 477.py
+++ 477.py
@@ -1,10 +1,14 @@
 class Solution:
     def totalHammingDistance(self, nums):
-        nums = [bin(n)[2:].rjust(32, '0') for n in nums]
         res = 0
         for i in range(32):
             one_num = 0
             for n in nums:
-                one_num += n[i] == '1'
+                one_num += (n >> i) & 1
             res += one_num * (len(nums) - one_num)
         return res
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    print(solution.totalHammingDistance([4, 14, 2]))
