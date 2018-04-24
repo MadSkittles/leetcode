027f6fc6f9b0ef3be88f822ae0a982a5246d7600
李偘文 <leekanwen@gmail.com>
2018-04-24 19:53:33 +0800

    Update 477.py

--- 477.py
+++ 477.py
@@ -2,11 +2,10 @@ class Solution:
     def totalHammingDistance(self, nums):
         res = 0
         for i in range(32):
-            one_num = 0
-            for n in nums:
-                one_num += (n >> i) & 1
+            one_num = sum((n >> i) & 1 for n in nums)
             res += one_num * (len(nums) - one_num)
         return res
+
 
 
 if __name__ == '__main__':
