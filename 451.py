dea4535e257aa3747a2508c9706e8db262b2f799
李偘文 <leekanwen@gmail.com>
2018-04-24 16:22:44 +0800

    Update 451.py

--- 451.py
+++ 451.py
@@ -3,3 +3,18 @@ class Solution:
         from collections import Counter
         c = Counter(s)
         return ''.join(sorted(s, key=lambda x: (c[x], x), reverse=True))
+
+    def frequencySort1(self, s):
+        from collections import Counter
+        res, c, bucket = '', Counter(s), [[] for _ in range(len(s) + 1)]
+        for k in c:
+            bucket[c[k]].append(k)
+        for i in range(len(s), 0, -1):
+            for ch in bucket[i]:
+                res += ch * i
+        return res
+
+
+if __name__ == '__main__':
+    solutioin = Solution()
+    print(solutioin.frequencySort1("loveleetcode"))
