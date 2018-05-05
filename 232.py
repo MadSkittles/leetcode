ea1c4727652ef4af5d19a11e0c40874aab1c8fc9
李偘文 <leekanwen@gmail.com>
2018-05-05 15:46:40 +0800

    Create 232.py

---
+++ 232.py
@@ -0,0 +1,22 @@
+class MyQueue:
+
+    def __init__(self):
+        self.stack1 = []
+        self.stack2 = []
+
+    def push(self, x):
+        self.stack1.append(x)
+
+    def pop(self):
+        while self.stack1:
+            self.stack2.append(self.stack1.pop())
+        res = self.stack2.pop()
+        while self.stack2:
+            self.stack1.append(self.stack2.pop())
+        return res
+
+    def peek(self):
+        return self.stack1[0]
+
+    def empty(self):
+        return len(self.stack1) == 0
