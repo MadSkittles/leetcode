7fd57dc63ea48575afac9209a7fca4ad15b0b9db
李偘文 <leekanwen@gmail.com>
2018-05-12 14:18:10 +0800

    Update 99.py

--- 99.py
+++ 99.py
@@ -3,28 +3,24 @@ from common import TreeNode
 
 class Solution:
     def recoverTree(self, root):
-        if not root:
-            return None
-        self.data, self.index = [], 0
-        self.f(root)
-        self.data.sort()
-        self.f(root, False)
+        self.first, self.second, self.pre = None, None, None
 
-    def f(self, node, record=True):
-        if node.left:
-            self.f(node.left, record)
-        if record:
-            self.data.append(node.val)
-        else:
-            node.val = self.data[self.index]
-            self.index += 1
-        if node.right:
-            self.f(node.right, record)
+        def traverse(node):
+            if not node: return
+            traverse(node.left)
+            if self.pre and self.pre.val >= node.val:
+                if not self.first:
+                    self.first = self.pre
+                self.second = node
+            self.pre = node
+            traverse(node.right)
+
+        traverse(root)
+        self.first.val, self.second.val = self.second.val, self.first.val
 
 
 if __name__ == '__main__':
     solution = Solution()
-    root = TreeNode.list2Tree([3, 1, 4, None, None, 2])
+    root = TreeNode.list2Tree([3, 4, 1, 5, 2])
     solution.recoverTree(root)
     print(root)
-    # print(solution.recoverTree(TreeNode.list2Tree([3, 1, 4, None, None, 2])))
