d3a94595699a83e487320a43ca07d133175924ba
李偘文 <leekanwen@gmail.com>
2018-05-21 15:13:54 +0800

    Update 300.py

--- 300.py
+++ 300.py
@@ -1,9 +1,9 @@
 class Solution:
     def lengthOfLIS(self, nums):
-        length = []
+        dp = []
         for i in range(len(nums)):
-            length.append(max([length[j] + 1 for j in range(i - 1, -1, -1) if nums[i] > nums[j]] + [1]))
-        return max(length + [0])
+            dp.append(max([dp[j] for j in range(i - 1, -1, -1) if nums[i] > nums[j]], default=0) + 1)
+        return max(dp, default=0)
 
     def lengthOfLIS1(self, nums):
         import bisect
@@ -15,8 +15,3 @@ class Solution:
             else:
                 tails[idx] = n
         return len(tails)
-
-
-if __name__ == '__main__':
-    solution = Solution()
-    print(solution.lengthOfLIS1([10, 9, 2, 5, 3, 7, 101, 18]))
