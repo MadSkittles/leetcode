4db935cbafcd4859bb35c45a17a142a9fe374f18
李偘文 <leekanwen@gmail.com>
2018-09-27 14:26:49 +0800

    Update heap_sort.py

--- others/heap_sort.py
+++ others/heap_sort.py
@@ -1,29 +1,27 @@
-def head_adjust(l, s, end=None):
-    end = len(l) if end is None else end
-    child = 2 * s + 1
-    while child < end:
-        if child < end - 1 and l[child] < l[child + 1]:
-            child += 1
-        if l[s] < l[child]:
-            l[s], l[child] = l[child], l[s]
-            s, child = child, 2 * child + 1
+def adjust_top(l, start=0, end=None):
+    cur, end = start, len(l) - 1 if end is None else end
+    while 2 * cur + 1 <= end:
+        child = 2 * cur + 1 + (2 * cur + 2 <= end and l[2 * cur + 2] > l[2 * cur + 1])
+        if l[child] > l[cur]:
+            l[cur], l[child] = l[child], l[cur]
+            cur = child
         else:
             break
 
 
 def heapify(l):
-    for i in range(len(l) // 2 - 1, -1, -1):
-        head_adjust(l, i)
+    for i in range(len(l) // 2)[::-1]:
+        adjust_top(l, start=i)
 
 
 def heap_sort(l):
     heapify(l)
-    for i in range(len(l) - 1, 0, -1):
+    for i in range(1, len(l))[::-1]:
         l[0], l[i] = l[i], l[0]
-        head_adjust(l, 0, i)
+        adjust_top(l, end=i - 1)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     l = [54, 2, 56, 12, 22, 46, 31, 67, 92, 52, 43, 54]
     heap_sort(l)
     print(l)
