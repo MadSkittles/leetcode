8023e4993b8c7baf294329c13e28cc003aaf1dd0
李偘文 <leekanwen@gmail.com>
2018-09-27 17:50:47 +0800

    Update 712.py

--- 712.py
+++ 712.py
@@ -1,21 +1,19 @@
-# 最小编辑距离的简化变形
+from itertools import accumulate
+
 
 class Solution:
     def minimumDeleteSum(self, s1, s2):
-        s1, s2 = [*map(ord, s1)], [*map(ord, s2)]
-        dp = [[0] * (len(s2) + 1) for _ in range(len(s1) + 1)]
-        s = 0
-        for i in range(len(s1)):
-            s += s1[i]
-            dp[i + 1][0] = s
-        s = 0
-        for i in range(len(s2)):
-            s += s2[i]
-            dp[0][i + 1] = s
-        for i in range(len(s1)):
-            for j in range(len(s2)):
-                if s1[i] == s2[j]:
-                    dp[i + 1][j + 1] = dp[i][j]
+        s1_ac, s2_ac = [0] + [*accumulate(map(ord, s1))], [0] + [*accumulate(map(ord, s2))]
+        dp = [[0 if i > 0 and j > 0 else (j == 0 and s1_ac[i]) or (i == 0 and s2_ac[j]) for j in range(len(s2) + 1)] for i in range(len(s1) + 1)]
+        for i, c1 in enumerate(s1, 1):
+            for j, c2 in enumerate(s2, 1):
+                if c1 == c2:
+                    dp[i][j] = dp[i - 1][j - 1]
                 else:
-                    dp[i + 1][j + 1] = min(dp[i][j + 1] + s1[i], dp[i + 1][j] + s2[j])
+                    dp[i][j] = min(dp[i - 1][j] + ord(c1), dp[i][j - 1] + ord(c2), dp[i - 1][j - 1] + ord(c1) + ord(c2))
         return dp[-1][-1]
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    print(solution.minimumDeleteSum("sea", "eat"))
