1508ebc765d55149df22c79d3ef066902f8c6c32
李偘文 <leekanwen@gmail.com>
2018-09-27 15:04:03 +0800

    Update 313.py

--- 313.py
+++ 313.py
@@ -1,24 +1,20 @@
-class Solution:
+import heapq
+
+
+class Solution(object):
     def nthSuperUglyNumber(self, n, primes):
-        ugly = [1]
-        index = [0] * len(primes)
-        umins = primes[::]
+        ugly, index, heap = [1], [0] * len(primes), [(v, i) for i, v in enumerate(primes)]
+        heapq.heapify(heap)
         while n > 1:
-            umin, umin_pos = float('inf'), 0
-            for i, v in enumerate(umins):
-                if v < umin:
-                    umin = v
-                    umin_pos = i
-            index[umin_pos] += 1
+            umin, umin_pos = heapq.heappop(heap)
             if umin > ugly[-1]:
                 ugly.append(umin)
                 n -= 1
-            umins[umin_pos] = primes[umin_pos] * ugly[index[umin_pos]]
+            index[umin_pos] += 1
+            heapq.heappush(heap, (primes[umin_pos] * ugly[index[umin_pos]], umin_pos))
         return ugly[-1]
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     solution = Solution()
-    print(solution.nthSuperUglyNumber(100000,
-                                      [7, 19, 29, 37, 41, 47, 53, 59, 61, 79, 83, 89, 101, 103, 109, 127, 131, 137, 139,
-                                       157, 167, 179, 181, 199, 211, 229, 233, 239, 241, 251]))
+    print(solution.nthSuperUglyNumber(100000, [7, 19, 29, 37, 41, 47, 53, 59, 61, 79, 83, 89, 101, 103, 109, 127, 131, 137, 139, 157, 167, 179, 181, 199, 211, 229, 233, 239, 241, 251]))
