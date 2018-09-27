a45cfb1eb6d2213c14114e9b95f639069b37521c
李偘文 <leekanwen@gmail.com>
2018-09-27 17:25:46 +0800

    Update 399.py

--- 399.py
+++ 399.py
@@ -1,5 +1,39 @@
 class Solution:
     def calcEquation(self, equations, values, queries):
+        self.union = {}
+        for i in range(len(equations)):
+            f, t, v = *equations[i], values[i]
+            if f not in self.union:
+                self.union[f] = (f, 1)
+            if t not in self.union:
+                self.union[t] = (t, 1)
+            root_f, root_f_val = self.union_find(f)
+            root_t, root_t_val = self.union_find(t)
+            if root_f != root_t:
+                self.union[root_t] = (root_f, v * root_f_val / root_t_val)
+
+        res = []
+        for f, t in queries:
+            root_f, root_f_val = self.union_find(f)
+            root_t, root_t_val = self.union_find(t)
+            if not root_f or not root_t or root_f != root_t:
+                res.append(-1.0)
+            else:
+                res.append(root_t_val / root_f_val)
+        return res
+
+    def union_find(self, node):
+        if node not in self.union:
+            return None, None
+        cur, mul = node, 1
+        while cur != self.union[cur][0]:
+            parent, val = self.union[cur]
+            mul *= val
+            cur = parent
+        self.union[node] = (cur, mul)
+        return cur, mul
+
+    def calcEquation1(self, equations, values, queries):
         division_map, graph = {}, {}
         for i in range(len(equations)):
             a, b = equations[i]
@@ -38,10 +72,4 @@ class Solution:
 
 if __name__ == "__main__":
     solution = Solution()
-    print(
-        solution.calcEquation(
-            [["a", "b"], ["b", "c"]],
-            [2.0, 3.0],
-            [["a", "c"], ["b", "a"], ["a", "e"], ["a", "a"], ["x", "x"]],
-        )
-    )
+    print(solution.calcEquation([["a", "b"], ["b", "c"]], [2.0, 3.0], [["a", "c"], ["b", "a"], ["a", "e"], ["a", "a"], ["x", "x"]]))
