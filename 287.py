b2f874a4f94a33b17fd117d5d2413e102377f6cb
李偘文 <leekanwen@gmail.com>
2018-09-27 16:37:25 +0800

    Update 287.py

--- 287.py
+++ 287.py
@@ -3,7 +3,7 @@ class Solution:
         start, end = 1, len(nums) - 1
         while start <= end:
             mid = (start + end) // 2
-            if len(list(filter(lambda x: x <= mid, nums))) <= mid:
+            if sum(map(lambda x: x <= mid, nums)) <= mid:
                 start = mid + 1
             else:
                 end = mid - 1
