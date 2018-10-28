4b8206ce9002aa2cf965438bf1db71cbc4ee7b9f
python大鹌鹑 <likanwen@icloud.com>
2018-10-28 22:33:44 +0800

    update

--- others/nim_game.py
+++ others/nim_game.py
@@ -1,7 +1,7 @@
 from functools import reduce
+from operator import xor
 
 
 # 共有n堆，每堆有a1、a2...an个，每次在一堆中至少取一个，先取光者输
 def nim_game(heaps):
-    return reduce(lambda x, y: x ^ y, heaps) != 0
-
+    return reduce(xor, heaps) != 0
