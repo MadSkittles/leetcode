817bbb73dfe095b9c9358dc459ba6605a2a9a256
李偘文 <leekanwen@gmail.com>
2021-11-12 11:28:47 +0800

    Update 31.py

--- 31.py
+++ 31.py
@@ -1,27 +1,17 @@
+from typing import List
+from bisect import bisect
+
+
 class Solution:
-    def nextPermutation(self, nums):
-        for i in range(len(nums) - 1, 0, -1):
+    def nextPermutation(self, nums: List[int]) -> None:
+        for i in range(1, len(nums))[::-1]:
             if nums[i] > nums[i - 1]:
-                break
-        else:
-            return self.reverse(nums)
-
-        point = i
-        for i in range(len(nums) - 1, point - 1, -1):
-            if nums[point - 1] < nums[i]:
-                self.insert(nums, i, point - 1)
+                nums[i:] = nums[i:][::-1]
+                pos = bisect(nums, nums[i - 1], i)
+                nums[i - 1], nums[pos] = nums[pos], nums[i - 1]
                 return
-
-    def reverse(self, nums, start=None, end=None):
-        start, end = start or 0, end or len(nums) - 1
-        while start < end:
-            nums[start], nums[end] = nums[end], nums[start]
-            start += 1
-            end -= 1
-
-    def insert(self, nums, pos, insert_pos):
-        nums[pos], nums[insert_pos] = nums[insert_pos], nums[pos]
-        self.reverse(nums, insert_pos + 1)
+        else:
+            nums[:] = nums[::-1]
 
 
 if __name__ == '__main__':
